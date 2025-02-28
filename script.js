73d43b47032886b83af3f4cfadc06c1ae38c838b
Abdallah Amr <abdallahamrkasper@gmail.com>
2025-02-28 22:53:35 +0300

    finish

--- script.js
+++ script.js
@@ -1,99 +1,131 @@
 const corect = document.getElementById("corect_ans");
 const false_ans = document.getElementById("false_ans");
 const define_ans = document.getElementById("define_ans");
+// const p3 = document.getElementById("p3");
+// const hent_btn = document.getElementById("hent_btn");
 // const corect2 = document.getElementById("corect_ans2");
 // const false_ans2 = document.getElementById("false_ans2");
 // const define_ans2 = document.getElementById("define_ans2");
+// const question_card_hedden = document.getElementById("question_card_hedden");
+
 
 const ans_buttons = document.querySelectorAll(".ans_buttons");
 // const ans_buttons2= document.querySelectorAll(".ans_buttons2");
+
 corect.style.display = "none";
 false_ans.style.display = "none";
 define_ans.style.display = "none";
+// p3.style.display = "none";
+//     hent_btn.addEventListener("click",function(){
+//         p3.style.display = "block";
+            
+//         })
+
+
+// let x = document.querySelector('.question-card')
+
+// x.remove()
+
 // corect2.style.display = "none";
 // false_ans2.style.display = "none";
 // define_ans2.style.display = "none";
+// const isHidden = localStorage.getItem('question_card_hedden_hidden');
+
+// if (isHidden === 'true') {
+//     question_card_hedden.style.display = 'block';
+// } else {
+//     question_card_hedden.style.display = 'none';
+    
+//     setTimeout(() => {
+//         question_card_hedden.style.display = 'block';
+//         localStorage.setItem('question_card_hedden_hidden', 'true');
+//     }, 6000);
+// }
+
+
+
 
 let score = 0;
-function checkTextAnswer() {
-const userAnswer = normalizeArabicText(document.getElementById('answerInput').value);
-const correctAnswers = ["الكعبة المشرفة", "الكعبة", "بيت الله الحرام"];
-const correctTextAnswer = document.getElementById('correctTextAnswer');
-const falseTextAnswer = document.getElementById('falseTextAnswer');
-const defineTextAnswer = document.getElementById('defineTextAnswer');
+// function checkTextAnswer() {
+// const userAnswer = normalizeArabicText(document.getElementById('answerInput').value);
+// const correctAnswers = ["  لقمان  "];
+// const correctTextAnswer = document.getElementById('correctTextAnswer');
+// const falseTextAnswer = document.getElementById('falseTextAnswer');
+// const defineTextAnswer = document.getElementById('defineTextAnswer');
 
-const isCorrect = correctAnswers.some(answer => normalizeArabicText(answer) === userAnswer);
+// const isCorrect = correctAnswers.some(answer => normalizeArabicText(answer) === userAnswer);
 
-if (isCorrect) {
-correctTextAnswer.style.display = "block";
-falseTextAnswer.style.display = "none";
-defineTextAnswer.style.display = "none";
-score += 25; 
-createConfetti();
-localStorage.setItem('textAnswerState', 'correct');
-} else {
-falseTextAnswer.style.display = "block";
-correctTextAnswer.style.display = "none";
-defineTextAnswer.style.display = "block";
-localStorage.setItem('textAnswerState', 'wrong');
-}
+// if (isCorrect) {
+// correctTextAnswer.style.display = "block";
+// falseTextAnswer.style.display = "none";
+// defineTextAnswer.style.display = "none";
+// score += 25; 
+// createConfetti();
+// localStorage.setItem('textAnswerState', 'correct');
+// } else {
+// falseTextAnswer.style.display = "block";
+// correctTextAnswer.style.display = "none";
+// defineTextAnswer.style.display = "block";
+// localStorage.setItem('textAnswerState', 'wrong');
+// }
 
-updateProgress(); 
-localStorage.setItem('score', score); 
-}
+// updateProgress(); 
+// localStorage.setItem('score', score); 
+// }
 
-function normalizeArabicText(text) {
-return text
-.trim()
-.toLowerCase()
-.replace(/[\u064B-\u065F]/g, '')
-.replace(/[أإآا]/g, 'ا')
-.replace(/[ىي]/g, 'ي')
-.replace(/ة/g, 'ه');
-}
-const submit_button = document.getElementById('submit_button');
+// function normalizeArabicText(text) {
+// return text
+// .trim()
+// .toLowerCase()
+// .replace(/[\u064B-\u065F]/g, '')
+// .replace(/[أإآا]/g, 'ا')
+// .replace(/[ىي]/g, 'ي')
+// .replace(/ة/g, 'ه');
+// }
+// const submit_button = document.getElementById('submit_button');
 
 
-submit_button.addEventListener('click', function() {
-submit_button.disabled = true;
-})
-window.addEventListener('load', () => {
-const answerInput = document.getElementById('answerInput');
-const submitButton = document.querySelector('.submit-button');
-const correctTextAnswer = document.getElementById('correctTextAnswer');
-const falseTextAnswer = document.getElementById('falseTextAnswer');
-const defineTextAnswer = document.getElementById('defineTextAnswer');
+// submit_button.addEventListener('click', function() {
+// submit_button.disabled = true;
+// })
+// window.addEventListener('load', () => {
+// const answerInput = document.getElementById('answerInput');
+// const submitButton = document.querySelector('.submit-button');
+// const correctTextAnswer = document.getElementById('correctTextAnswer');
+// const falseTextAnswer = document.getElementById('falseTextAnswer');
+// const defineTextAnswer = document.getElementById('defineTextAnswer');
 
-if (localStorage.getItem('textAnswerSubmitted') === 'true') {
-answerInput.disabled = true;
-submitButton.disabled = true;
+// if (localStorage.getItem('textAnswerSubmitted') === 'true') {
+// answerInput.disabled = true;
+// submitButton.disabled = true;
 
-if (localStorage.getItem('textAnswerCorrect') === 'true') {
-    correctTextAnswer.style.display = "block";
-    falseTextAnswer.style.display = "none";
-    defineTextAnswer.style.display = "none";
-} else {
-    falseTextAnswer.style.display = "block";
-    correctTextAnswer.style.display = "none";
-    defineTextAnswer.style.display = "block";
-}
-}
-const savedTextAnswer = localStorage.getItem('textAnswerState');
-if (savedTextAnswer === 'correct') {
-document.getElementById('correctTextAnswer').style.display = 'block';
-} else if (savedTextAnswer === 'wrong') {
-document.getElementById('falseTextAnswer').style.display = 'block';
-document.getElementById('defineTextAnswer').style.display = 'block';
-}
-});
+// if (localStorage.getItem('textAnswerCorrect') === 'true') {
+//     correctTextAnswer.style.display = "block";
+//     falseTextAnswer.style.display = "none";
+//     defineTextAnswer.style.display = "none";
+// } else {
+//     falseTextAnswer.style.display = "block";
+//     correctTextAnswer.style.display = "none";
+//     defineTextAnswer.style.display = "block";
+// }
+// }
+// const savedTextAnswer = localStorage.getItem('textAnswerState');
+// if (savedTextAnswer === 'correct') {
+// document.getElementById('correctTextAnswer').style.display = 'block';
+// } else if (savedTextAnswer === 'wrong') {
+// document.getElementById('falseTextAnswer').style.display = 'block';
+// document.getElementById('defineTextAnswer').style.display = 'block';
+// }
+// });
 function resetAllData() {
-    document.getElementById('answerInput').value = '';
-document.getElementById('correctTextAnswer').style.display = 'none';
-document.getElementById('falseTextAnswer').style.display = 'none';
-document.getElementById('defineTextAnswer').style.display = 'none';
+//     document.getElementById('answerInput').value = '';
+// document.getElementById('correctTextAnswer').style.display = 'none';
+// document.getElementById('falseTextAnswer').style.display = 'none';
+// document.getElementById('defineTextAnswer').style.display = 'none';
         localStorage.clear();
         
         score = 0;
+// p3.style.display = "none";
         
         const tasks = document.querySelectorAll('.task-card input[type="checkbox"]');
         tasks.forEach(task => {
@@ -107,8 +139,8 @@ document.getElementById('defineTextAnswer').style.display = 'none';
             button.disabled = false;
             
         });
-        const submit_button = document.getElementById('submit_button');
-        submit_button.disabled = false;
+        // const submit_button = document.getElementById('submit_button');
+        // submit_button.disabled = false;
         
 
 
@@ -126,10 +158,15 @@ document.getElementById('defineTextAnswer').style.display = 'none';
        ans_buttons.forEach(button => {
        button.style.backgroundColor = '#8b5e3c';
 
-    
    });
-        // corect2.style.display = "none";
-        // false_ans2.style.display = "none";
+//    ans_buttons2.forEach(button2 => {
+//     button2.style.backgroundColor = '#8b5e3c';
+
+// });
+//         corect2.style.display = "none";
+//         false_ans2.style.display = "none";
+//         define_ans2.style.display = "none";
+
         updateProgress();
         
         removeCelebrationRibbons();
@@ -140,9 +177,7 @@ document.getElementById('defineTextAnswer').style.display = 'none';
     }
 
 // const resetButton = document.getElementById('resetButton');
-// // دالة لإعادة تعيين البيانات
 
-// // حدث النقر على الزر
 // resetButton.addEventListener('click', function () {
 //     resetAllData();
 //     resetButton.style.display = "none"; 
@@ -154,24 +189,24 @@ document.getElementById('defineTextAnswer').style.display = 'none';
 //         resetButton.style.display = "none"; 
 //     } else {
 //         resetButton.style.display = "block"; 
-//     
+//     }
 // });
 
-const resetButton2 = document.getElementById('resetButton2');
+// const resetButton2 = document.getElementById('resetButton2');
 
-resetButton2.addEventListener('click', function () {
-    resetAllData(); 
-    resetButton2.style.display = "none"; 
-    localStorage.setItem('resetButtonState2', 'hidden2');
-});
-document.addEventListener('DOMContentLoaded', function () {
-    const buttonState2 = localStorage.getItem('resetButtonState2');
-    if (buttonState2 === 'hidden2') {
-        resetButton2.style.display = "none";    
-    } else {
-        resetButton2.style.display = "block"; 
-    }
-});
+// resetButton2.addEventListener('click', function () {
+//     resetAllData(); 
+//     resetButton2.style.display = "none"; 
+//     localStorage.setItem('resetButtonState2', 'hidden2');
+// });
+// document.addEventListener('DOMContentLoaded', function () {
+//     const buttonState2 = localStorage.getItem('resetButtonState2');
+//     if (buttonState2 === 'hidden2') {
+//         resetButton2.style.display = "none";    
+//     } else {
+//         resetButton2.style.display = "block"; 
+//     }
+// });
 
 
 window.addEventListener('load', () => {
@@ -215,19 +250,19 @@ window.addEventListener('load', () => {
 
     }
 
-//     const savedCorrectAnswer2 = localStorage.getItem('correctAnswer2');
-//     if (savedCorrectAnswer2 === 'shown2') {
+    // const savedCorrectAnswer2 = localStorage.getItem('correctAnswer2');
+    // if (savedCorrectAnswer2 === 'shown2') {
        
-//         corect2.style.display = "block";
-//         false_ans2.style.display = "none";
-//         define_ans2.style.display = "none";
-//     } else if (savedCorrectAnswer2 === 'wrong2') {
+    //     corect2.style.display = "block";
+    //     false_ans2.style.display = "none";
+    //     define_ans2.style.display = "none";
+    // } else if (savedCorrectAnswer2 === 'wrong2') {
         
-//         false_ans2.style.display = "block";
-//         corect2.style.display = "none";
-//         define_ans2.style.display = "block";
+    //     false_ans2.style.display = "block";
+    //     corect2.style.display = "none";
+    //     define_ans2.style.display = "block";
 
-//     }
+    // }
 });
 
 function checkAnswer(button, status) {
@@ -274,10 +309,10 @@ function checkAnswer(button, status) {
 
 //     }
     
-//     ans_buttons2.forEach(btn2 => {
-//         btn2.disabled = true;
-//         localStorage.setItem(btn2.textContent.trim(), 'clicked');
-//     });
+//     // ans_buttons2.forEach(btn2 => {
+//     //     btn2.disabled = true;
+//     //     localStorage.setItem(btn2.textContent.trim(), 'clicked');
+//     // });
     
 //     updateProgress();
 //     localStorage.setItem('score', score);
@@ -297,6 +332,7 @@ function updateProgress() {
     progress.style.width = percentage + '%';
     progress.innerText = Math.round(percentage) + '%';
     let pointsEarned = (percentage / 100 * 5).toFixed(1);
+   
     pointsDisplay.innerText = pointsEarned + ' نقطة من 5 نقاط';
 
     if (percentage === 100) {
@@ -432,6 +468,16 @@ if (this.checked) {
         }
     });
 
+    // const buttons2 = document.querySelectorAll('.ans_buttons2');
+    // buttons2.forEach(button2 => {
+    //     const buttonId2 = button2.textContent.trim();
+    //     const savedButtonState2 = localStorage.getItem(buttonId2);
+    //     if (savedButtonState2 === 'clicked') {
+    //         button2.disabled = true;
+            
+    //     }
+    // });
+
     const checkboxess = document.querySelectorAll('input[type="checkbox"]');
 window.addEventListener('load', () => {
 checkboxess.forEach(checkbox => {
@@ -482,5 +528,107 @@ taskCards.forEach(card => {
 card.style.opacity = "100%"; 
 });
 }
+
+const hijriDateElement = document.getElementById('hijri-date');
+const hijriDateHeader = document.getElementById('hijri-date-header');
+const today = new Date();
+
+today.setDate(today.getDate() - 1);
+
+const hijriDate = new Intl.DateTimeFormat('ar-SA-u-ca-islamic', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+}).format(today);
+
+hijriDateHeader.textContent = hijriDate;
+
+hijriDateElement.textContent = hijriDate;
+ 
+
+// setTimeout(() => {
+//     submit_button.disabled = true;
+//     const defineTextAnswer = document.getElementById('defineTextAnswer');
+//     defineTextAnswer.style.display = "block"
+
+//     localStorage.setItem('submit_button', 'true');
+//     localStorage.setItem('defineTextAnswer', 'block');
+// }, 20000);
+
+// window.addEventListener('load', () => {
+// const   submit_button =   localStorage.getItem('submit_button', 'true');
+// const   defineTextAnswer =  localStorage.getItem('defineTextAnswer', 'block');
+    
+
+//     if (submit_button === 'true') {
+//         submit_button.disabled = true;
+        
+//     }
+
+//     if (defineTextAnswer === 'block') {
+//        const defineTextAnswer = document.getElementById('defineTextAnswer');
+//         defineTextAnswer.style.display = "block"
+//     }
+// });
+
+ 
+
+
+// setTimeout(() => {
+//     const buttons2 = document.querySelectorAll('.ans_buttons2');
+//     buttons2.forEach(button2 => {
+//         button2.disabled = true;
+//     });
+//     define_ans2.style.display = 'block';
+
+//     localStorage.setItem('buttonsDisabled2', 'true');
+//     localStorage.setItem('defineAnsDisplay2', 'block');
+// }, 20000);
+
+// window.addEventListener('load', () => {
+//     const buttonsDisabled2 = localStorage.getItem('buttonsDisabled2');
+//     const defineAnsDisplay2 = localStorage.getItem('defineAnsDisplay2');
+
+//     if (buttonsDisabled2 === 'true') {
+//         const buttons2 = document.querySelectorAll('.ans_buttons2');
+//         buttons2.forEach(button2 => {
+//             button2.disabled = true;
+//         });
+//     }
+
+//     if (defineAnsDisplay2 === 'block') {
+//         define_ans2.style.display = 'block';
+//     }
+// });
+
+ 
+
+
+  setTimeout(() => {
+    const buttons = document.querySelectorAll('.ans_buttons');
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+    define_ans.style.display = 'block';
+
+    localStorage.setItem('buttonsDisabled', 'true');
+    localStorage.setItem('defineAnsDisplay', 'block');
+}, 20000);
+
+window.addEventListener('load', () => {
+    const buttonsDisabled = localStorage.getItem('buttonsDisabled');
+    const defineAnsDisplay = localStorage.getItem('defineAnsDisplay');
+
+    if (buttonsDisabled === 'true') {
+        const buttons = document.querySelectorAll('.ans_buttons');
+        buttons.forEach(button => {
+            button.disabled = true;
+        });
+    }
+
+    if (defineAnsDisplay === 'block') {
+        define_ans.style.display = 'block';
+    }
+});
 
 // localStorage.clear()
