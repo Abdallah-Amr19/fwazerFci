def1fb39ceb4f06b2935fedae4a6f03541d4fb33
Abdallah Amr <abdallahamrkasper@gmail.com>
2025-03-04 22:52:05 +0300

    update

--- script.js
+++ script.js
@@ -6,14 +6,14 @@
 // const define_ansR = document.getElementById("define_ansR");
 const p3 = document.getElementById("p3");
 const hent_btn = document.getElementById("hent_btn");
-// const corect2 = document.getElementById("corect_ans2");
-// const false_ans2 = document.getElementById("false_ans2");
-// const define_ans2 = document.getElementById("define_ans2");
+const corect2 = document.getElementById("corect_ans2");
+const false_ans2 = document.getElementById("false_ans2");
+const define_ans2 = document.getElementById("define_ans2");
 // const question_card_hedden = document.getElementById("question_card_hedden");
 
 // const ans_buttons = document.querySelectorAll(".ans_buttons");
 // const ans_buttonsR = document.querySelectorAll(".ans_buttonsR");
-// const ans_buttons2= document.querySelectorAll(".ans_buttons2");
+const ans_buttons2= document.querySelectorAll(".ans_buttons2");
 // corect.style.display = "none";
 // false_ans.style.display = "none";
 // define_ans.style.display = "none";
@@ -31,9 +31,9 @@ p3.style.display = "none";
 
 // x.remove()
 
-// corect2.style.display = "none";
-// false_ans2.style.display = "none";
-// define_ans2.style.display = "none";
+corect2.style.display = "none";
+false_ans2.style.display = "none";
+define_ans2.style.display = "none";
 // const isHidden = localStorage.getItem('question_card_hedden_hidden');
 
 // if (isHidden === 'true') {
@@ -154,11 +154,11 @@ p3.style.display = "none";
         
 
 
-        // const buttons2 = document.querySelectorAll('.ans_buttons2');
-        // buttons2.forEach(button2 => {
-        //     button2.disabled = false;
+        const buttons2 = document.querySelectorAll('.ans_buttons2');
+        buttons2.forEach(button2 => {
+            button2.disabled = false;
             
-        // });
+        });
         
         
         // corect.style.display = "none";
@@ -172,13 +172,13 @@ p3.style.display = "none";
 //        buttonR.style.backgroundColor = '#8b5e3c';
 
 //    });
-//    ans_buttons2.forEach(button2 => {
-//     button2.style.backgroundColor = '#8b5e3c';
+   ans_buttons2.forEach(button2 => {
+    button2.style.backgroundColor = '#8b5e3c';
 
-// });
-        // corect2.style.display = "none";
-        // false_ans2.style.display = "none";
-        // define_ans2.style.display = "none";
+});
+        corect2.style.display = "none";
+        false_ans2.style.display = "none";
+        define_ans2.style.display = "none";
 
         updateProgress();
         
@@ -239,14 +239,14 @@ window.addEventListener('load', () => {
     });
    
 
-    // const buttons2 = document.querySelectorAll('.ans_buttons2');
-    // buttons2.forEach(button2 => {
-    //     const buttonId2 = button2.textContent.trim();
-    //     const savedButtonState2 = localStorage.getItem(buttonId2);
-    //     if (savedButtonState2 === 'clicked') {
-    //         button2.disabled = true;
-    //     }
-    // });
+    const buttons2 = document.querySelectorAll('.ans_buttons2');
+    buttons2.forEach(button2 => {
+        const buttonId2 = button2.textContent.trim();
+        const savedButtonState2 = localStorage.getItem(buttonId2);
+        if (savedButtonState2 === 'clicked') {
+            button2.disabled = true;
+        }
+    });
 
     // const savedCorrectAnswer = localStorage.getItem('correctAnswer');
     // if (savedCorrectAnswer === 'shown') {
@@ -274,19 +274,19 @@ window.addEventListener('load', () => {
 
     // }
 
-    // const savedCorrectAnswer2 = localStorage.getItem('correctAnswer2');
-    // if (savedCorrectAnswer2 === 'shown2') {
+    const savedCorrectAnswer2 = localStorage.getItem('correctAnswer2');
+    if (savedCorrectAnswer2 === 'shown2') {
        
-    //     corect2.style.display = "block";
-    //     false_ans2.style.display = "none";
-    //     define_ans2.style.display = "none";
-    // } else if (savedCorrectAnswer2 === 'wrong2') {
+        corect2.style.display = "block";
+        false_ans2.style.display = "none";
+        define_ans2.style.display = "none";
+    } else if (savedCorrectAnswer2 === 'wrong2') {
         
-    //     false_ans2.style.display = "block";
-    //     corect2.style.display = "none";
-    //     define_ans2.style.display = "block";
+        false_ans2.style.display = "block";
+        corect2.style.display = "none";
+        define_ans2.style.display = "block";
 
-    // }
+    }
 });
 
 // function checkAnswer(button, status) {
@@ -339,33 +339,33 @@ window.addEventListener('load', () => {
 //     updateProgress();
 //     localStorage.setItem('score', score);
 // }
-// function checkAnswer2(button2, status2) {
-//     if (status2 === 'صح') {
-//         score += 25;
+function checkAnswer2(button2, status2) {
+    if (status2 === 'صح') {
+        score += 25;
         
-//         corect2.style.display = "block";
-//         false_ans2.style.display = "none";
-//         localStorage.setItem('correctAnswer2', 'shown2');
-//         button2.style.backgroundColor = '#2E8B57';
-//         createConfetti();
-//     } else {
+        corect2.style.display = "block";
+        false_ans2.style.display = "none";
+        localStorage.setItem('correctAnswer2', 'shown2');
+        button2.style.backgroundColor = '#2E8B57';
+        createConfetti();
+    } else {
         
-//         false_ans2.style.display = "block";
-//         corect2.style.display = "none";
-//         localStorage.setItem('correctAnswer2', 'wrong2');
-//         button2.style.backgroundColor = '#DC143C';
-//         define_ans2.style.display = "block";
+        false_ans2.style.display = "block";
+        corect2.style.display = "none";
+        localStorage.setItem('correctAnswer2', 'wrong2');
+        button2.style.backgroundColor = '#DC143C';
+        define_ans2.style.display = "block";
 
-//     }
+    }
     
-//     ans_buttons2.forEach(btn2 => {
-//         btn2.disabled = true;
-//         localStorage.setItem(btn2.textContent.trim(), 'clicked');
-//     });
+    ans_buttons2.forEach(btn2 => {
+        btn2.disabled = true;
+        localStorage.setItem(btn2.textContent.trim(), 'clicked');
+    });
     
-//     updateProgress();
-//     localStorage.setItem('score', score);
-// }
+    updateProgress();
+    localStorage.setItem('score', score);
+}
 
 function updateScore(points) {
     score += points;
@@ -527,15 +527,15 @@ if (this.checked) {
     //     }
     // });
 
-    // const buttons2 = document.querySelectorAll('.ans_buttons2');
-    // buttons2.forEach(button2 => {
-    //     const buttonId2 = button2.textContent.trim();
-    //     const savedButtonState2 = localStorage.getItem(buttonId2);
-    //     if (savedButtonState2 === 'clicked') {
-    //         button2.disabled = true;
+    const buttons2 = document.querySelectorAll('.ans_buttons2');
+    buttons2.forEach(button2 => {
+        const buttonId2 = button2.textContent.trim();
+        const savedButtonState2 = localStorage.getItem(buttonId2);
+        if (savedButtonState2 === 'clicked') {
+            button2.disabled = true;
             
-    //     }
-    // });
+        }
+    });
 
     const checkboxess = document.querySelectorAll('input[type="checkbox"]');
 window.addEventListener('load', () => {
@@ -633,32 +633,32 @@ const   defineTextAnswer =  localStorage.getItem('defineTextAnswer', 'block');
  
 
 
-// setTimeout(() => {
-//     const buttons2 = document.querySelectorAll('.ans_buttons2');
-//     buttons2.forEach(button2 => {
-//         button2.disabled = true;
-//     });
-//     define_ans2.style.display = 'block';
+setTimeout(() => {
+    const buttons2 = document.querySelectorAll('.ans_buttons2');
+    buttons2.forEach(button2 => {
+        button2.disabled = true;
+    });
+    define_ans2.style.display = 'block';
 
-//     localStorage.setItem('buttonsDisabled2', 'true');
-//     localStorage.setItem('defineAnsDisplay2', 'block');
-// }, 25000);
+    localStorage.setItem('buttonsDisabled2', 'true');
+    localStorage.setItem('defineAnsDisplay2', 'block');
+}, 25000);
 
-// window.addEventListener('load', () => {
-//     const buttonsDisabled2 = localStorage.getItem('buttonsDisabled2');
-//     const defineAnsDisplay2 = localStorage.getItem('defineAnsDisplay2');
+window.addEventListener('load', () => {
+    const buttonsDisabled2 = localStorage.getItem('buttonsDisabled2');
+    const defineAnsDisplay2 = localStorage.getItem('defineAnsDisplay2');
 
-//     if (buttonsDisabled2 === 'true') {
-//         const buttons2 = document.querySelectorAll('.ans_buttons2');
-//         buttons2.forEach(button2 => {
-//             button2.disabled = true;
-//         });
-//     }
+    if (buttonsDisabled2 === 'true') {
+        const buttons2 = document.querySelectorAll('.ans_buttons2');
+        buttons2.forEach(button2 => {
+            button2.disabled = true;
+        });
+    }
 
-//     if (defineAnsDisplay2 === 'block') {
-//         define_ans2.style.display = 'block';
-//     }
-// });
+    if (defineAnsDisplay2 === 'block') {
+        define_ans2.style.display = 'block';
+    }
+});
 
  
 
