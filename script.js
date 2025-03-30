25f7adcdfc3254f28e3c7b9e744f96247d7337e7
Abdallah Amr <abdallahamrkasper@gmail.com>
2025-03-30 21:55:33 +0200

    السوال الأخير

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// const corect = document.getElementById("corect_ans");
-// const false_ans = document.getElementById("false_ans");
-// const define_ans = document.getElementById("define_ans");
+const corect = document.getElementById("corect_ans");
+const false_ans = document.getElementById("false_ans");
+const define_ans = document.getElementById("define_ans");
 const corectR = document.getElementById("corect_ansR");
 const false_ansR = document.getElementById("false_ansR");
 const define_ansR = document.getElementById("define_ansR");
@@ -10,12 +10,12 @@ const define_ansR = document.getElementById("define_ansR");
 // const false_ans2 = document.getElementById("false_ans2");
 // const define_ans2 = document.getElementById("define_ans2");
 // const question_card_hedden = document.getElementById("question_card_hedden");
-// const ans_buttons = document.querySelectorAll(".ans_buttons");
+const ans_buttons = document.querySelectorAll(".ans_buttons");
 const ans_buttonsR = document.querySelectorAll(".ans_buttonsR");
 // const ans_buttons2= document.querySelectorAll(".ans_buttons2");
-// corect.style.display = "none";
-// false_ans.style.display = "none";
-// define_ans.style.display = "none";
+corect.style.display = "none";
+false_ans.style.display = "none";
+define_ans.style.display = "none";
 corectR.style.display = "none";
 false_ansR.style.display = "none";
 define_ansR.style.display = "none";
@@ -135,11 +135,11 @@ function resetAllData() {
           
         });
         
-        // const buttons = document.querySelectorAll('.ans_buttons');
-        // buttons.forEach(button => {
-        //     button.disabled = false;
+        const buttons = document.querySelectorAll('.ans_buttons');
+        buttons.forEach(button => {
+            button.disabled = false;
             
-        // });
+        });
         const buttonsR = document.querySelectorAll('.ans_buttonsR');
         buttonsR.forEach(buttonR => {
             buttonR.disabled = false;
@@ -157,9 +157,9 @@ function resetAllData() {
         // });
         
         
-        // corect.style.display = "none";
-        // false_ans.style.display = "none";
-        // define_ans.style.display = "none";
+        corect.style.display = "none";
+        false_ans.style.display = "none";
+        define_ans.style.display = "none";
         corectR.style.display = "none";
         false_ansR.style.display = "none";
         define_ansR.style.display = "none";
@@ -246,17 +246,17 @@ window.addEventListener('load', () => {
     //     }
     // });
 
-    // const savedCorrectAnswer = localStorage.getItem('correctAnswer');
-    // if (savedCorrectAnswer === 'shown') {
-    //     corect.style.display = "block";
-    //     false_ans.style.display = "none";
-    //     define_ans.style.display = "none";
+    const savedCorrectAnswer = localStorage.getItem('correctAnswer');
+    if (savedCorrectAnswer === 'shown') {
+        corect.style.display = "block";
+        false_ans.style.display = "none";
+        define_ans.style.display = "none";
        
-    // } else if (savedCorrectAnswer === 'wrong') {
-    //     false_ans.style.display = "block";
-    //     corect.style.display = "none";
-    //     define_ans.style.display = "block";
-    // }
+    } else if (savedCorrectAnswer === 'wrong') {
+        false_ans.style.display = "block";
+        corect.style.display = "none";
+        define_ans.style.display = "block";
+    }
 
     const savedCorrectAnswerR = localStorage.getItem('correctAnswerR');
     if (savedCorrectAnswerR === 'shownR') {
@@ -287,31 +287,31 @@ window.addEventListener('load', () => {
     // }
 });
 
-// function checkAnswer(button, status) {
-//     if (status === 'صح') {
-//         score += 25;
-//         corect.style.display = "block";
-//         false_ans.style.display = "none";
-//         localStorage.setItem('correctAnswer', 'shown');
-//         button.style.backgroundColor = '#2E8B57';
-//         createConfetti();
-//     } else {
-//         false_ans.style.display = "block";
-//         corect.style.display = "none";
-//         localStorage.setItem('correctAnswer', 'wrong');
-//         button.style.backgroundColor = '#DC143C';
-//         define_ans.style.display = "block";
+function checkAnswer(button, status) {
+    if (status === 'صح') {
+        score += 25;
+        corect.style.display = "block";
+        false_ans.style.display = "none";
+        localStorage.setItem('correctAnswer', 'shown');
+        button.style.backgroundColor = '#2E8B57';
+        createConfetti();
+    } else {
+        false_ans.style.display = "block";
+        corect.style.display = "none";
+        localStorage.setItem('correctAnswer', 'wrong');
+        button.style.backgroundColor = '#DC143C';
+        define_ans.style.display = "block";
 
-//     }
+    }
     
-//     ans_buttons.forEach(btn => {
-//         btn.disabled = true;
-//         localStorage.setItem(btn.textContent.trim(), 'clicked');
-//     });
+    ans_buttons.forEach(btn => {
+        btn.disabled = true;
+        localStorage.setItem(btn.textContent.trim(), 'clicked');
+    });
     
-//     updateProgress();
-//     localStorage.setItem('score', score);
-// }
+    updateProgress();
+    localStorage.setItem('score', score);
+}
 function checkAnswerR(buttonR, statusR) {
     if (statusR === 'صح') {
         score += 25;
@@ -505,15 +505,15 @@ if (this.checked) {
 });
 });
 
-    // const buttons = document.querySelectorAll('.ans_buttons');
-    // buttons.forEach(button => {
-    //     const buttonId = button.textContent.trim();
-    //     const savedButtonState = localStorage.getItem(buttonId);
-    //     if (savedButtonState === 'clicked') {
-    //         button.disabled = true;
+    const buttons = document.querySelectorAll('.ans_buttons');
+    buttons.forEach(button => {
+        const buttonId = button.textContent.trim();
+        const savedButtonState = localStorage.getItem(buttonId);
+        if (savedButtonState === 'clicked') {
+            button.disabled = true;
             
-    //     }
-    // });
+        }
+    });
 
     const buttonsR = document.querySelectorAll('.ans_buttonsR');
     buttonsR.forEach(buttonR => {
